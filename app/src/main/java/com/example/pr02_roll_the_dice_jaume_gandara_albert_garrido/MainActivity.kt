17916282fda5f26be813efa08f38d75f09703f86
JaumeGandara <jaumegandara@gmail.com>
2024-10-23 18:10:01 +0200

    add background and padding

--- app/src/main/java/com/example/pr02_roll_the_dice_jaume_gandara_albert_garrido/MainActivity.kt
+++ app/src/main/java/com/example/pr02_roll_the_dice_jaume_gandara_albert_garrido/MainActivity.kt
@@ -25,6 +25,8 @@ import kotlin.random.Random
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Icon
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.layout.ContentScale
 import com.example.pr02_roll_the_dice_jaume_gandara_albert_garrido.ui.theme.Pr02rollthedicejaumegandaraalbertgarridoTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,123 +48,52 @@ fun MainLayout(modifier: Modifier = Modifier) {
     var diceIndex1 by remember { mutableStateOf(1) }
     var diceIndex2 by remember { mutableStateOf(1) }
     var credits by remember { mutableStateOf(10) }
+    var botonsDown by remember { mutableStateOf(false) }
     val context = LocalContext.current
 
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.SpaceBetween,
-        horizontalAlignment = Alignment.CenterHorizontally
+    Box (
+        modifier = modifier.fillMaxSize().padding()
     ) {
         Image(
-            painter = painterResource(id = R.drawable.lsg_logo),
-            contentDescription = "Imatge de títol",
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(300.dp)
+            painter = painterResource(id = R.drawable.tapestry),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
         )
 
-        Row {
-            Icon(
-                imageVector = Icons.Filled.Star,
-                contentDescription = "Icona credits",
-                tint = Color.Red
-            )
-            Text(text = "$credits")
-        }
-
-        Button(onClick = {
-            if (credits > 0) {
-                credits -= 1
-                val handler = Handler(Looper.getMainLooper())
-
-                val runnable = object : Runnable {
-                    override fun run() {
-                        diceIndex1 = getRandomDice()
-                        diceIndex2 = getRandomDice()
-                        handler.postDelayed(this, 100)
-                    }
-                }
-
-                handler.post(runnable)
-
-                handler.postDelayed({
-                    handler.removeCallbacks(runnable)
-                    diceIndex1 = getRandomDice()
-                    diceIndex2 = getRandomDice()
-
-                    if (diceIndex1 == diceIndex2) {
-                        Toast.makeText(context, "JACKPOT!", Toast.LENGTH_SHORT).show()
-                        credits += if (diceIndex1 == 6) {
-                            10
-                        } else {
-                            5
-                        }
-                    }
-                }, 2000)
-            }
-        }, modifier = Modifier.fillMaxWidth()) {
-            Text(text = "Tirar daus")
-        }
-
-        Button(onClick = {
-            diceIndex1 = 1
-            diceIndex2 = 7
-            credits = 10
-        }, modifier = Modifier.fillMaxWidth()) {
-            Text(text = "Reiniciar joc")
-        }
-
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Image(
+                painter = painterResource(id = R.drawable.lsg_logo),
+                contentDescription = "Imatge de títol",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp)
+            )
+
+            Row {
+                Icon(
+                    imageVector = Icons.Filled.Star,
+                    contentDescription = "Icona credits",
+                    tint = Color.Red
+                )
+                Text(text = "$credits")
+            }
+
             Button(onClick = {
-                if (credits > 0) {
-                    credits -= 2
+                if (credits > 0 && !botonsDown) {
+                    credits -= 1
+                    botonsDown = true
                     val handler = Handler(Looper.getMainLooper())
 
                     val runnable = object : Runnable {
                         override fun run() {
                             diceIndex1 = getRandomDice()
-                            handler.postDelayed(this, 100)
-                        }
-                    }
-
-                    handler.post(runnable)
-
-                    handler.postDelayed({
-                        handler.removeCallbacks(runnable)
-                        diceIndex1 = getRandomDice()
-
-                        if (diceIndex1 == diceIndex2) {
-                            Toast.makeText(context, "JACKPOT!", Toast.LENGTH_SHORT).show()
-                            credits += if (diceIndex1 == 6) {
-                                10
-                            } else {
-                                5
-                            }
-                        }
-                    }, 2000)
-                }
-            }, modifier = Modifier.weight(1f),
-                contentPadding = PaddingValues(0.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),) {
-                Image(
-                    painter = painterResource(id = getDiceImage(diceIndex1)),
-                    contentDescription = "Dau 1",
-                    modifier = Modifier.size(144.dp)
-                )
-            }
-            Spacer(modifier = Modifier.width(16.dp))
-            Button(onClick = {
-                if (credits > 0) {
-                    credits -= 2
-                    val handler = Handler(Looper.getMainLooper())
-
-                    val runnable = object : Runnable {
-                        override fun run() {
                             diceIndex2 = getRandomDice()
                             handler.postDelayed(this, 100)
                         }
@@ -172,6 +103,7 @@ fun MainLayout(modifier: Modifier = Modifier) {
 
                     handler.postDelayed({
                         handler.removeCallbacks(runnable)
+                        diceIndex1 = getRandomDice()
                         diceIndex2 = getRandomDice()
 
                         if (diceIndex1 == diceIndex2) {
@@ -182,17 +114,110 @@ fun MainLayout(modifier: Modifier = Modifier) {
                                 5
                             }
                         }
+                        botonsDown = false
                     }, 2000)
                 }
-            }, modifier = Modifier.weight(1f),
-                contentPadding = PaddingValues(0.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+            }, modifier = Modifier.fillMaxWidth()) {
+                Text(text = "Tirar daus")
+            }
+
+            Button(onClick = {
+                diceIndex1 = 1
+                diceIndex2 = 1
+                credits = 10
+            }, modifier = Modifier.fillMaxWidth()) {
+                Text(text = "Reiniciar joc")
+            }
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Image(
-                    painter = painterResource(id = getDiceImage(diceIndex2)),
-                    contentDescription = "Dau 2",
-                    modifier = Modifier.size(144.dp)
-                )
+                Button(
+                    onClick = {
+                        if (credits > 1 && !botonsDown) {
+                            credits -= 2
+                            botonsDown = true
+                            val handler = Handler(Looper.getMainLooper())
+
+                            val runnable = object : Runnable {
+                                override fun run() {
+                                    diceIndex1 = getRandomDice()
+                                    handler.postDelayed(this, 100)
+                                }
+                            }
+
+                            handler.post(runnable)
+
+                            handler.postDelayed({
+                                handler.removeCallbacks(runnable)
+                                diceIndex1 = getRandomDice()
+
+                                if (diceIndex1 == diceIndex2) {
+                                    Toast.makeText(context, "JACKPOT!", Toast.LENGTH_SHORT).show()
+                                    credits += if (diceIndex1 == 6) {
+                                        10
+                                    } else {
+                                        5
+                                    }
+                                }
+                                botonsDown = false
+                            }, 2000)
+                        }
+                    },
+                    modifier = Modifier.weight(1f),
+                    contentPadding = PaddingValues(0.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+                ) {
+                    Image(
+                        painter = painterResource(id = getDiceImage(diceIndex1)),
+                        contentDescription = "Dau 1",
+                        modifier = Modifier.size(144.dp)
+                    )
+                }
+                Spacer(modifier = Modifier.width(16.dp))
+                Button(
+                    onClick = {
+                        if (credits > 1 && !botonsDown) {
+                            credits -= 2
+                            botonsDown = true
+                            val handler = Handler(Looper.getMainLooper())
+
+                            val runnable = object : Runnable {
+                                override fun run() {
+                                    diceIndex2 = getRandomDice()
+                                    handler.postDelayed(this, 100)
+                                }
+                            }
+
+                            handler.post(runnable)
+
+                            handler.postDelayed({
+                                handler.removeCallbacks(runnable)
+                                diceIndex2 = getRandomDice()
+
+                                if (diceIndex1 == diceIndex2) {
+                                    Toast.makeText(context, "JACKPOT!", Toast.LENGTH_SHORT).show()
+                                    credits += if (diceIndex1 == 6) {
+                                        10
+                                    } else {
+                                        5
+                                    }
+                                }
+                                botonsDown = false
+                            }, 2000)
+                        }
+                    },
+                    modifier = Modifier.weight(1f),
+                    contentPadding = PaddingValues(0.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+                ) {
+                    Image(
+                        painter = painterResource(id = getDiceImage(diceIndex2)),
+                        contentDescription = "Dau 2",
+                        modifier = Modifier.size(144.dp)
+                    )
+                }
             }
         }
     }
